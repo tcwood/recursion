6531fce1bfc7a4701764c3a7154dc740c4e85369
Travis Wood <travis_cw@yahoo.com>
2016-08-08 15:34:27 -0700

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,37 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var contents = [];
+  if(Array.isArray(obj)) {
+  	if (obj.length === 0) {
+  		return "[]";
+  	} else {
+  		_.forEach(obj, function(value){
+  			contents.push(stringifyJSON(value));
+  		});
+	  	return "[" + contents.toString() + "]";
+	}
+  }
+  else if(typeof obj === 'object' && obj !== null) {
+  	if (_.isEmpty(obj)) {
+  		return "{}";
+  	} else {
+  		_.forEach(obj, function(value, key){
+  			if (!(typeof value === 'function' || typeof value === 'undefined')) {
+	  			contents.push('"' + key +'":' + stringifyJSON(value));
+  			}
+  		});
+  		return "{" + contents.toString() + "}";
+  	}
+  }
+  else if (!isNaN(parseFloat(obj)) && isFinite(obj)) {
+  	return obj.toString();
+  }
+  else if (obj === null || obj === true || obj === false) {
+  	return String(obj);
+  }
+  else if(typeof obj === "string") {
+  	return '"' + obj + '"';
+  }
 };
